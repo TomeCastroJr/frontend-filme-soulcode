2c2fa72f48160aa6dedf1225246680584b77341c
William dos Santos Rodrigues <williambfs2011@gmail.com>
2024-08-08 16:38:54 -0300

    correçao novo filme e diretor

--- src/pages/Diretor.jsx
+++ src/pages/Diretor.jsx
@@ -16,64 +16,67 @@ const Diretor = () => {
       setDiretor(dados);
     })
   }
-  function deletarDiretor(id){
+  function deletarDiretor(id) {
     const deletar = confirm("Tem certeza que deseja excluir o diretor e seus filmes ?")
-    if(deletar) {
+    if (deletar) {
       deleteDiretor(id)
-      .then((resposta) => {
-        toast.success(resposta.message);
-        carregarDiretor();
+        .then((resposta) => {
+          toast.success(resposta.message);
+          carregarDiretor();
 
-  })}}
-;  
+        })
+    }
+  }
+  ;
   useEffect(() => {
     carregarDiretor();
-  }, [])
+  }, []);
+  
   return (
     <main className="mt-4 container">
-    <h1>Diretor</h1>
-    <Button as={Link} to="/diretor/novo">
+      <h1>Diretor</h1>
+      <Button as={Link} to="/diretor/novo">
         Adicionar Diretor
-    </Button>
-    {
-        diretor ? 
-        <Table>
-                    <thead>
-            <tr>
-              <th>Id</th>
-              <th>Nome</th>
-              <th>Nascimento</th>
-              <th>Nacionalidade</th>
-              <th>Ação</th>
-            </tr>
-            {
-              diretor.map( (diretor) => {
-                return (
-                  <tr key={diretor.id}>
-                    <td>{ diretor.id }</td>
-                    <td>{ diretor.nome }</td>
-                    <td>{ diretor.nascimento }</td>
-                    <td>{ diretor.nacionalidade }</td>
-                   
-                    <td>
-                      <div className="d-flex">
-                        <Button size="sm" variant="danger" as={Link} className="mx-2" onClick={()=> deletarDiretor(diretor.id) } >Excluir</Button>
-                        <Button size="sm" as={Link} to={`/diretor/editar/${diretor.id}`}>
-                          Editar
-                        </Button>
-                      </div>
-                    </td>
-                  </tr>
-                )
-              })
-            }
-          </thead>
-        </Table>:
-        <Loader />
+      </Button>
+      {
+        diretor ?
+          <Table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Nascimento</th>
+                <th>Nacionalidade</th>
+                <th>Ação</th>
+              </tr>
+              {
+                diretor.map((diretor) => {
+                  return (
+                    <tr key={diretor.id}>
+                      <td>{diretor.id}</td>
+                      <td>{diretor.nome}</td>
+                      <td>{diretor.nascimento}</td>
+                      <td>{diretor.nacionalidade}</td>
+
+                      <td>
+                        <div className="d-flex">
+                          <Button size="sm" variant="danger" as={Link} className="mx-2" onClick={() => deletarDiretor(diretor.id)} >Excluir</Button>
+                          <Button size="sm" as={Link} to={`/diretor/editar/${diretor.id}`}>
+                            Editar
+                          </Button>
+                        </div>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+            </thead>
+          </Table> :
+          <Loader />
       }
 
-    <hr />
-</main>
+      <hr />
+    </main>
   )
 }
 
